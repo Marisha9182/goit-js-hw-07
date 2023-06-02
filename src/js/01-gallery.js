772d165fc503f9452e29483d7153418683528e67
Marisha9182 <120337365+Marisha9182@users.noreply.github.com>
2023-06-02 17:05:11 +0200

    task 1-2 html

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,13 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-console.log(galleryItems);
-// 1.Создание и рендер разметки по массиву данных galleryItems
-// и предоставленному шаблону элемента галереи.
-const galleryMarkupContainer = document.querySelector('.gallery');
-const galleryMarkup = createGalleryMarkup(galleryItems);
-galleryMarkupContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-function createGalleryMarkup(items) {
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+function createGalleryItemsMarkup(items) {
   return items
-    .map(({ original, preview, description }) => {
+    .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -23,39 +21,29 @@ function createGalleryMarkup(items) {
     })
     .join('');
 }
-// 2.Реализация делегирования на div.gallery и получение url большого изображения.
-// 4.Открытие модального окна по клику на элементе галереи.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
-// 5.Изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу.Запрети это поведение по умолчанию.
-galleryMarkupContainer.addEventListener('click', onOpenModalWindow);
 
-function onOpenModalWindow(e) {
-  e.preventDefault();
-
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-  const gallery = basicLightbox.create(
-    `
-	<img width="1200" height="700" src="${e.target.dataset.source}">
-`,
-    {
-      closable: true,
-      onShow: gallery => {
-        window.addEventListener('keydown', onEsckeyPress);
-      },
-
-      onClose: gallery => {
-        window.removeEventListener('keydown', onEsckeyPress);
-      },
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
     },
-  );
-  gallery.show();
-
-  function onEsckeyPress(e) {
-    if (e.code === 'Escape') {
-      gallery.close();
-      console.log('gallery = ', gallery);
-    }
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
   }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
 }
