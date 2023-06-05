d257611ce5f6b21dd9af97d113863bc1b9896ae2
Marisha9182 <120337365+Marisha9182@users.noreply.github.com>
2023-06-05 10:19:27 +0200

    corect

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,42 +8,40 @@ galleryContainer.addEventListener('click', onImgClick);
 function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                  <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                  />
+                </a>
+              </li>`;
     })
     .join('');
 }
 
-const instance = basicLightbox.create(
-  `
-<img width="1280" height="auto" src="">`,
-  {
-    onShow: (instance) => {
-      window.addEventListener('keydown', onEscKeyPress);
-    },
-    onClose: (instance) => {
-      window.removeEventListener('keydown', onEscKeyPress);
-    },
-  }
-);
 
 function onImgClick(e) {
   e.preventDefault();
-  const datasetSource = e.target.dataset.source;
-  if (!datasetSource) return;
-  instance.element().querySelector('img').src = datasetSource;
-  instance.show();
+  const path = e.target.dataset.source;
+  const description = e.target.alt;
+
+  const instance = basicLightbox.create(
+    `<img src="${path}" width="1280" height="auto" alt="${description}">`,
+    {
+      onShow: () => {
+        window.addEventListener('keydown', (event) => onEscKeyPress(event, instance));
+      },
+      onClose: () => {
+        window.removeEventListener('keydown',  (event) => onEscKeyPress(event, instance));
+      },
+    });
+    instance.show();
 }
 
-function onEscKeyPress(e) {
+function onEscKeyPress(e, instance) {
   if (e.code !== 'Escape') return;
   instance.close();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryMarkupContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
@@ -9,9 +9,11 @@ galleryMarkupContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 function createGalleryMarkup(items) {
   return items
     .map(({ original, preview, description }) => {
-      return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+              </li>`;
     })
     .join('');
 }
